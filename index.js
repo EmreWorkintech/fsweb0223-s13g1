6e2056d4572fd9a4465350d1750848e6a44dd3f2
EmreSahiner <emre@journeyapp.com>
2023-05-18 12:03:34 +0300

    wip

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const server = require('./api/server');
 const { PORT } = require('./config/config');
- 
 
+console.log("env:", process.env.PORT);
+console.log(PORT);
 server.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`);
 })
